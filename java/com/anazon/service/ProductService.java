39e6ab6a6ad042895bb72a23decddec167623036
Asem Alnajjar <Asem@Asems-MacBook-Pro.local>
2019-02-08 22:27:08 -0600

    first commit

---
+++ java/com/anazon/service/ProductService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anazon.service;
+
+import com.anazon.model.Product;
+import java.util.List;
+
+
+public interface ProductService {
+
+    public List<Product> getAll();
+
+    public Product add(Product product);
+
+    public Product get(long id);
+
+    public Product update(Product product);
+
+    public Product delete(Product product);
+
+    public void deleteAll(List<Product> selectedProducts);
+
+    public List<Product> getProductsByCategoryId(int categoryId);
+}
